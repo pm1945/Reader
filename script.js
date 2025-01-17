138b93c1b7eb30dd7bfb708fbd64a673cabcde60
pm1945 <pm1945@yandex.ru>
2025-01-17 19:29:40 +0300

    Вывод данных файла с расчетом длины стен

--- script.js
+++ script.js
@@ -33,13 +33,32 @@ fileInput.addEventListener("change", async (event) => {
                     l1: wall.l1,
                     l2: wall.l2,
                     r1: wall.r1,
-                    r2: wall.r2
+                    r2: wall.r2,
+                    depth: wall.depth,
+                    height: wall.height,
+                    neighbors: wall.neighbors,
+                    lnx: wall.l2.y - wall.l1.y,
+                    lny: wall.l2.x - wall.l1.x 
                 });
             }
         }
 
         // Display extracted data
-        output.textContent = JSON.stringify(walls, null, 2);
+        output.innerHTML = walls.map(wall => {
+            return `<div>
+                        <strong>Wall ID:</strong> ${wall.id}<br>
+                        <strong>Coordinates:</strong><br>
+                        &nbsp;&nbsp;l1: ${JSON.stringify(wall.l1)}<br>
+                        &nbsp;&nbsp;l2: ${JSON.stringify(wall.l2)}<br>
+                        &nbsp;&nbsp;r1: ${JSON.stringify(wall.r1)}<br>
+                        &nbsp;&nbsp;r2: ${JSON.stringify(wall.r2)}<br>
+                        &nbsp;&nbsp;depth: ${JSON.stringify(wall.depth)}<br>
+                        &nbsp;&nbsp;height: ${JSON.stringify(wall.height)}<br>
+                        &nbsp;&nbsp;neighbors: ${JSON.stringify(wall.neighbors)}<br>
+                        &nbsp;&nbsp;lnx: ${JSON.stringify(wall.lnx)}<br>
+                        &nbsp;&nbsp;lny: ${JSON.stringify(wall.lny)}<br>
+                    </div><hr>`;
+        }).join("");
     } catch (error) {
         output.textContent = `Error processing file: ${error.message}`;
     }
